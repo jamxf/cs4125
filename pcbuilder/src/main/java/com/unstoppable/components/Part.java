908a0cc5c15c9a23adf8333f8cb05223d3fd0c76
James <james_foley@me.com>
2019-12-01 21:52:41 +0000

    new order stuff

---
+++ pcbuilder/src/main/java/com/unstoppable/components/Part.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unstoppable.components;
+
+/**
+ *
+ * @author jamesfoley
+ */
+public class Part {
+    
+    private Double price;
+    
+    public Part(){
+        
+    }
+    
+    
+    public Double getPrice(){
+        return this.price;
+    }
+}
