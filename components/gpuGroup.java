8f7e5afc1dac2c3d8c4a575bc6e643a2c003d255
iansidway <iansidway@gmail.com>
2019-12-02 00:03:01 +0100

    adding Componenets folder

---
+++ components/gpuGroup.java
@@ -0,0 +1,49 @@
+import java.util.Iterator;
+import java.util.ArrayList;
+public class gpuGroup extends Part{
+    
+    private String groupname;
+    private String groupDescription;
+    private ArrayList group = new ArrayList();
+
+
+    public gpuGroup(){
+        this.groupname = "";
+
+
+    }
+    public gpuGroup(String gn, String gd){
+        this.gn = groupname;
+        this.gd = groupDescription;
+    }
+
+    /**
+     * @return Group Name
+     */
+    public int getGroupName() {
+        return groupName;
+    }
+
+    /**
+     * @return the group description
+     */
+    public String getGroupDesc() {
+        return groupDescription;
+    }
+
+    /**
+     * @return the compatability
+     */
+    public void addGpu(Part newgpu) {
+        group.add(newgpu);
+    }
+
+    public void removeGpur(Part newgpu){
+        group.remove(newgpu);
+    }
+
+    public Part getComponent(int partIndex){
+        return (Part)group.get(partIndex);
+    }
+
+}
