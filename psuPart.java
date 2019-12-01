3e8d448e9811b57f888f4b2f185b9f0f609f0a06
iansidway <iansidway@gmail.com>
2019-12-02 00:43:43 +0100

    CPU, Drive, PSU, RAM parts added

---
+++ psuPart.java
@@ -0,0 +1,108 @@
+public class psuPart extends Part{
+    
+    private int partID;
+    private String name;
+    private String category;
+    private String description;
+    private Double price;
+    private String status;
+    private int stock;
+    private int compatability;
+    private String Vendor;
+    private int cpacity;
+    private String rating;
+
+
+    public psuPart(){
+
+
+    }
+    
+    public psuPart(int id, String name, String category, String description, Double price, String status, int compatability, String V, String c, String r){
+        this.partID = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.status = status;
+        this.compatability = compatability;
+        this.Vendor = V;
+        this.cpacity = c;
+        this.rating= r;
+
+    }
+
+    public void add(Part newPart){
+        throw new UnsupportedOperationException();
+    }
+    public void remove(Part newPart){
+        throw new UnsupportedOperationException();
+    }
+    public Part getPart(int partIndex){
+        throw new UnsupportedOperationException();
+    }
+
+    public String getRating(){
+        return rating;
+    }
+    public int getCapcity(){
+        return capacity;
+    }
+
+    /**
+     * @return the partID
+     */
+    public int getPartID() {
+        return partID;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @return the compatability
+     */
+    public int getCompatability() {
+        return compatability;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @return the price
+     */
+    public Double getPrice() {
+        return price;
+    }
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+    /**
+     * @return the stock
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    public String getVendor() {
+        return Vendor;
+    }
+    
+}
