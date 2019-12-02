ad24faa1c2d92eb475d7d2d7fa824d131f61eddd
James <james_foley@me.com>
2019-12-02 08:53:36 +0000

    9am

---
+++ pcbuilder/src/main/java/com/unstoppable/store/StoreTester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unstoppable.store;
+
+/**
+ *
+ * @author jamesfoley
+ */
+public class StoreTester {
+    
+    public static void main(String [] args){
+       PreBuilt pres = new PreBuilt();
+       pres.getBuilds();
+       pres.printBuilds();
+    }
+}
