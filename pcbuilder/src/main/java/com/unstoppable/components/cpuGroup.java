13168aa3ff094cd0b06df5e0462d07608d63793b
iansidway <iansidway@gmail.com>
2019-12-02 01:26:14 +0100

    newGroups

---
+++ pcbuilder/src/main/java/com/unstoppable/components/cpuGroup.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unstoppable.components;
+import java.util.Iterator;
+import java.util.ArrayList;
+/**
+ *
+ * @author Iansi
+ */
+public class cpuGroup {
+    private String groupName;
+    private String groupDescription;
+    private ArrayList group = new ArrayList();
+
+
+    public cpuGroup(){
+        this.groupName = "";
+
+
+    }
+    public cpuGroup(String gn, String gd){
+        this.groupName = gn;
+        this.groupDescription = gd;
+    }
+
+    /**
+     * @return Group Name
+     */
+    public String getGroupName() {
+        return groupName;
+    }
+    
+    public String getGroupDesc() {
+        return groupDescription;
+    }
+    
+    public void addGpu(Part newPart) {
+        group.add(newPart);
+    }
+
+    public void removeGpur(Part newPart){
+        group.remove(newPart);
+    }
+
+    public Part getComponent(int partIndex){
+        return (Part)group.get(partIndex);
+    }    
+}
